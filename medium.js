6ee620441e6ef025ba31b65308fb5eb670ac934a
Gius3ppe95 <Snak3Ey3s.2023@gmail.com>
2023-10-27 16:26:58 +0200

    S5/L5-medium page-progetto

---
+++ medium.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", function () {
+  let navbar = document.querySelector(".navbar");
+  let nav_button = document.querySelector(".nav_button");
+  if (window.scrollY > 500) {
+    navbar.style.backgroundColor = "white";
+    nav_button.style.backgroundColor = "#1a8917";
+    navbar.style.transition = "background-color 1s ease";
+    nav_button.style.transition = "background-color 1s ease";
+    navbar.style.transition = "background-color 0.7s ease";
+    nav_button.style.transition = "background-color 0.7s ease";
+  } else {
+    navbar.style.backgroundColor = "#ffc017";
+    nav_button.style.backgroundColor = "#191919";
+    navbar.style.transition = "background-color 1s ease";
+    nav_button.style.transition = "background-color 1s ease";
+    navbar.style.transition = "background-color 0.7s ease";
+    nav_button.style.transition = "background-color 0.7s ease";
+  }
+});
